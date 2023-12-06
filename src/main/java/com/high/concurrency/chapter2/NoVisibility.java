5d40947090188bf37b0b95230644169ae8b1b354
Max Qiu <yueyiqiu178@gmail.com>
2023-12-06 22:45:23 +0800

    修正NoVisibility.java

--- src/main/java/com/high/concurrency/chapter2/NoVisibility.java
+++ src/main/java/com/high/concurrency/chapter2/NoVisibility.java
@@ -7,16 +7,15 @@ package com.high.concurrency.chapter2;
  */
 public class NoVisibility {
 
-    private static boolean ready;
+    private volatile static boolean ready;
 
     private static int number;
 
     private static class ReaderThread extends Thread {
         @Override
         public void run() {
-            while (!ready) {
-                System.out.println(number);
-            }
+            while (!ready);
+            System.out.println(number);
         }
     }
 
