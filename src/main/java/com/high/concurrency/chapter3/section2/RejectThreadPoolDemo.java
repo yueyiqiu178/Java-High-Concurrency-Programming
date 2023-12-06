be24967be4d38a26a690a84a81ec0c93e349f724
Max Qiu <yueyiqiu178@gmail.com>
2023-12-06 22:08:30 +0800

    加入程式碼

---
+++ src/main/java/com/high/concurrency/chapter3/section2/RejectThreadPoolDemo.java
@@ -0,0 +1,61 @@
+package com.high.concurrency.chapter3.section2;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Description
+ * @Author dongzonglei
+ * @Date 2018/12/27 下午5:49
+ */
+public class RejectThreadPoolDemo {
+
+    public static class MyTask implements Runnable {
+
+        public void run() {
+            System.out.println(System.currentTimeMillis() + ":Thread ID" + Thread.currentThread().getId());
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+//        MyTask task = new MyTask();
+//        ExecutorService exec = new ThreadPoolExecutor(5, 5, 0L,
+//                TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(10),
+//                Executors.defaultThreadFactory(), new RejectedExecutionHandler() {
+//            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+//                System.out.println(r.toString() + " is discard");
+//            }
+//        });
+//        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+//            exec.submit(task);
+//            Thread.sleep(10);
+//        }
+
+        MyTask task = new MyTask();
+        ExecutorService es = new ThreadPoolExecutor(5, 5, 0L,
+                TimeUnit.MILLISECONDS, new SynchronousQueue<Runnable>(), new ThreadFactory() {
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                System.out.println("create " + t);
+                return t;
+            }
+        });
+
+        for (int i = 0; i < 5; i++) {
+            es.submit(task);
+        }
+        Thread.sleep(2000);
+    }
+}
