1672414810d7774932e4b1f36bf585f50f92a8dd
MaxQiu <yueyiqiu178@gmail.com>
2023-12-08 11:33:00 +0800

    update SimpleDateFormatDemo.java

--- src/main/java/com/high/concurrency/chapter4/section4/SimpleDateFormatDemo.java
+++ src/main/java/com/high/concurrency/chapter4/section4/SimpleDateFormatDemo.java
@@ -7,6 +7,8 @@ import java.util.concurrent.Executors;
 
 public class SimpleDateFormatDemo {
 
+	static volatile ThreadLocal<SimpleDateFormat> t1 = new ThreadLocal<SimpleDateFormat>();
+	
 	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	public static class ParseDate implements Runnable {
@@ -20,7 +22,14 @@ public class SimpleDateFormatDemo {
 		@Override
 		public void run() {
 			try {
-				Date t = sdf.parse("2015-03-29 19:29:" + i % 60);
+				
+                if (t1.get() == null) {
+                    t1.set(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+                    System.out.println(Thread.currentThread().getId() + ":create SimpleDateFormat");
+                }
+				
+				Date t = t1.get().parse("2015-03-29 19:29:" + i % 60);
+				//Date t = sdf.parse("2015-03-29 19:29:" + i % 60);
 				System.out.println(i + ":" + t);
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -33,5 +42,6 @@ public class SimpleDateFormatDemo {
 		for (int i = 0; i < 1000; i++) {
 			es.execute(new ParseDate(i));
 		}
+		es.shutdown();
 	}
 }
